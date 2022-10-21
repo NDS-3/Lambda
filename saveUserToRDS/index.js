f5f1ae6152525c8a736617c63a6a1c4019f01103
Wonyeong Choi <coma01155@gmail.com>
2022-10-21 21:16:53 +0900

    mysql -> mysql2 라이브러리 변경

--- saveUserToRDS/index.js
+++ saveUserToRDS/index.js
@@ -1,22 +1,21 @@
-const mysql = require('mysql');
+const mysql = require('mysql2/promise');
 
-const con = mysql.createConnection({
+const config = {
     host: process.env.RDS_HOSTNAME,
     user: process.env.RDS_USERNAME,
     password: process.env.RDS_PASSWORD,
     port: process.env.RDS_PORT,
     database: process.env.RDS_DATABASE
-});
+};
+
+const connection = await mysql.createConnection(config);
 
 exports.handler = async (event) => {
 
     const { email, name } = event.request.userAttributes;
     const sql = `INSERT INTO owners(email, username) VALUES('${email}', '${name}')`;
 
-    con.query(sql, function (err, result) {
-        if (err) throw err;
-        console.log(`${name} Created`);
-    });
+    await connection.execute(sql);
 
     return {
         "message": "User Created"
